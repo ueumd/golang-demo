c758d1f9098d39085de5f7b4cc4e698f8611b552
hsd <nicecoder@qq.com>
2019-02-27 08:39:55 +0800

    add

--- demo-io-os/file.go
+++ demo-io-os/file.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"fmt"
 	"time"
-)
+	)
 
 /**
 func Create(name string) (file *File, err error)  // Create采用模式0666（任何人都可读写，不可执行）创建一个名为name的文件，如果文件已存在会截断它（为空文件）
@@ -86,6 +86,100 @@ func f1()  {
 	// os.Create("./demo-io-os/hello.txt")
 }
 
+// 文件和文件夹操作函数
+func f2() {
+
+	// 创建目录
+	// perm权限设置，os.ModePerm为0777
+	err := os.Mkdir("./demo-io-os/tmp", os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 创建多级目录
+	err2 := os.MkdirAll("./demo-io-os/a/b/c", os.ModePerm)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+
+	// 等个3秒，看目录是否创建成功
+	time.Sleep(time.Second * 5)
+
+	//删除目录
+	//如果目录下有文件或其他目录会出错
+	err3 := os.Remove("./demo-io-os/tmp")
+	if err3 != nil {
+		fmt.Println(err3)
+	}
+
+
+	//删除多级目录
+	//如果是单个名称，则删除所有子目录
+	err4 := os.RemoveAll("./demo-io-os/a/b/c")
+	if err4 != nil {
+		fmt.Println(err4)
+	}
+
+	//文件操作
+	//创建文件
+	//Create会返回一个文件对象，默认权限0666
+	file1, err5 := os.Create("./demo-io-os/1.log")
+	defer file1.Close()
+	if err5 != nil {
+		fmt.Println(err5)
+	}
+
+	//通过文件描述符(文件句柄)来创建文件
+	file2 := os.NewFile(file1.Fd(), "./demo-io-os/2.log")
+	defer file2.Close()
+
+	//打开文件
+	//Open内部调用OpenFile，以只读方式打开
+
+	f3, err6 := os.Open("./demo-io-os/1.txt")
+	if err6 != nil {
+		fmt.Println(err6)
+	}
+	defer f3.Close()
+
+	//OpenFile指定文件打开方式，只读，只写，读写和权限
+	f4, err7 := os.OpenFile("./demo-io-os/1.txt", os.O_RDWR, 0666)
+	if err7 != nil {
+		fmt.Println(err7)
+	}
+	defer f4.Close()
+
+	f4.Write([]byte("写入数据\n"))
+
+	//在指定位置写入数据
+	f4.WriteAt([]byte("指定位置写入数据\n"), 1024)
+
+	f4.WriteString("直接写入数据\n")
+
+	//读取文件数据
+
+	//将文件偏移量设置为开头
+	f4.Seek(0, 0)
+	tmp := make([]byte, 256)
+	for n, _ := f4.Read(tmp); n!= 0; n, _ =f4.Read(tmp) {
+		fmt.Println(string(tmp))
+	}
+
+	//ReadAt从off指定位置读取数据到[]byte中
+	f4.Seek(0, 0)
+	var off int64 = 0
+	for {
+		n, _ := f4.ReadAt(tmp, off)
+		off += int64(n)
+		if n == 0 {
+			break
+		}
+
+		fmt.Println(string(tmp))
+	}
+
+}
+
 func main()  {
-	f1()
+	f2()
 }
